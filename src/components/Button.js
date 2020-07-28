6485d7e21a55e29aeb84b65d0e96156400474ec2
Prakash Acharya <pracharya2601@gmail.com>
2020-07-28 12:00:47 -0700

    initial setup

---
+++ src/components/Button.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default Button = ({ text }) => {
+  return <button>{text}</button>;
+};
---
+++ styleguide.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+      ],
+    },
+  },
+};
