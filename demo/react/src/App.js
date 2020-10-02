f3664aad0cccd5cef4f9aaff14c34caa5d1780cf
unknown <pracharya2601@gmail.com>
2020-10-01 23:32:02 -0700

    refactor and ass demo file

---
+++ demo/react/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from 'react-ui'
+const App = () => (
+  <div>
+    <Button title='hello btn' onClick={() => console.log('clicked')} />
+  </div>
+)
+
+export default App
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-const Button = ({ children }) => {
-  return <div>{children}</div>
+const StyledButton = styled.button`
+  cursor: pointer;
+  background: #7a7aff;
+  color: white;
+  margin: 1em;
+  font-size: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  border: 1px solid #7a7aff;
+
+  &:hover {
+    background: #a2a2fc;
+    color: #383838;
+    border: 1px solid #7a7aff;
+  }
+`
+
+const Button = ({ icon, title, onClick }) => (
+  <StyledButton onClick={onClick}>{title}</StyledButton>
+)
+
+Button.propTypes = {
+  title: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  icon: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
 }
 
 export default Button
